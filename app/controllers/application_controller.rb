fc5fce5f20af159cd31a7328b34614609ad03876
Augusto Granitto <augustogranitto@gmail.com>
2018-09-11 23:08:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def "Hello World" 
-  end 
+  def "Hello World"
+  end
 
-  
+
 end
